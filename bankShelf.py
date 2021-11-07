2cf8637bc99ea6e9b2de1c7dcd96ee30ceb79811
Mils <55298904+milenakalita@users.noreply.github.com>
2021-11-07 08:57:27 +0000

    Update bankShelf.py

--- bankShelf.py
+++ bankShelf.py
@@ -5,24 +5,36 @@ class BankShelf:
     def update(self, username, key, value):
         # updating account key values
         # for incrementing failed login counter you have to first use the select function to retrieve the current value
-        s = shelve.open('bankShelf', writeback=True)
-        s[username][key] = value
-        s.close()
+        try:
+            s = shelve.open('bankShelf', writeback=True)
+            s[username][key] = value
+            s.close()
+            return username, key, value # returns username, key and new value if operation successful
+        except:
+            return 'Error' # returns 'Error' if operation unsuccessful
 
     def insert(self, username, password):
         # inserting new account into shelf
-        s = shelve.open('bankShelf', writeback=True)
-        s[username] = {'password': password, 'balance': 0, 'frozen': False, 'failed_login': 0}
-        s.close()
+        try:
+            s = shelve.open('bankShelf', writeback=True)
+            s[username] = {'password': password, 'balance': 0, 'frozen': False, 'failed_login': 0}
+            s.close()
+            return username
+        except:
+            return 'Error'
+
 
     def select(self, username, key=None):
         # this function returns values of keys OR returns a a username if exists in shelf
-        s = shelve.open('bankShelf')
-        value = None
-        if key:
-            value = s[username][key]
-        else:
-            if username in s.keys():
-                value = username
-        s.close()
-        return value
+        try:
+            s = shelve.open('bankShelf')
+            value = None
+            if key:
+                value = s[username][key]
+            else:
+                if username in s.keys():
+                    value = username
+            s.close()
+            return value
+        except:
+            return 'Error'
