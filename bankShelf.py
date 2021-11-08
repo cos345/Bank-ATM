1788b1a632b431e8c48e075cec099f6b66274e17
Mils <55298904+milenakalita@users.noreply.github.com>
2021-11-08 12:42:57 +0000

    Update bankShelf.py
    added in checking if account exists before inserting, selecting and updating.

--- bankShelf.py
+++ bankShelf.py
@@ -7,26 +7,33 @@ class BankShelf:
         # for incrementing failed login counter you have to first use the select function to retrieve the current value
         try:
             s = shelve.open('bankShelf', writeback=True)
-            s[username][key] = value
+            if username in s.keys():  # check if username exists first
+                s[username][key] = value
+                result = username
+            else:  # returns None if username doesn't exist
+                result = None
             s.close()
-            return username, key, value # returns username, key and new value if operation successful
+            return result  # returns username, key and new value if operation successful
         except:
-            return 'Error' # returns 'Error' if operation unsuccessful
+            return 'Error'  # returns 'Error' if operation unsuccessful
 
     def insert(self, username, password):
         # inserting new account into shelf
         try:
             s = shelve.open('bankShelf', writeback=True)
-            s[username] = {'password': password, 'balance': 0, 'frozen': False, 'failed_login': 0}
+            if username not in s.keys():
+                s[username] = {'password': password, 'balance': 0, 'frozen': False, 'failed_login': 0}
+                result = username
+            else:  # username already exists
+                result = None
             s.close()
-            return username
+            return result
         except:
             return 'Error'
 
-
     def select(self, username, key=None):
         # this function returns values of keys OR returns a a username if exists in shelf
-        # returns None is usernam doesn't exist.
+        # returns None is username doesn't exist.
         # returns username if exists and no key is provided.
         # returns value if username exists and key is provided.
         try:
