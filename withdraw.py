fcca8f17b8c31e02a496a63969b4dbef7d44d094
cos345 <89390133+cos345@users.noreply.github.com>
2021-11-10 18:38:02 +0000

    Update withdraw.py

--- withdraw.py
+++ withdraw.py
@@ -7,11 +7,11 @@ def withdraw(username):
     account_bal = bankSystem.select(username, 'balance')
     withdraw_amount = withdraw.get()  # GETTING USER INPUT FROM ENTRY
     # Check amount vs account balance
-    if withdraw_amount > account_bal:
+    if not withdraw_amount.isnumeric() or withdraw_amount < 20 or withdraw_amount > 1000:
+        message = "Please enter an integer between 20 and 1000."
+    elif withdraw_amount > account_bal:
         message = "Amount exceeds current balance."
     # Check if amount is valid
-    elif withdraw_amount == "":
-        message = "Invalid Amount"
     # Else successfully withdraw amount
     else:
         account_bal -= withdraw_amount
