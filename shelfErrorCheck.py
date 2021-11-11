31265f73644160da4098f620c7ec9255064b60e0
Mils <55298904+milenakalita@users.noreply.github.com>
2021-11-11 17:20:45 +0000

    Delete shelfErrorCheck.py
    in ATM.py

--- shelfErrorCheck.py
+++
@@ -1,9 +0,0 @@
-def shelfErrorCheck(result):
-    if result is "Error":
-        message = "System Error"
-    elif not result:
-        message = "Data Error"
-    else:
-        message = None
-    return message
-
